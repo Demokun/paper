71c9f6d7e7015a57e5e45a0a80f0d7b6e32bc887
Demo <1012858388@qq.com>
2019-06-19 15:53:19 +0800

    2-12

--- loadFile.py
+++
@@ -1,9 +0,0 @@
-import os
-filePath = 'D:\data'
-os.listdir(filePath)
-for info in os.listdir(filePath):
-    domain = os.path.abspath(filePath)
-    info = os.path.join(domain,info)
-    info = open(info,'r')
-    print(info.read())
-    info.close()
